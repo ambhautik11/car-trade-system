7a5d6b090dfed3e60760784805ec8a9cb0c581d8
ambhautik11 <amipara.bhautik11@gmail.com>
2015-11-17 18:21:26 +0530

    added new comment
    lol lol lol learning git

--- java 3rd sem/Carsystem_14BIT006_14BIT002_14BIT012/abc.java
+++ java 3rd sem/Carsystem_14BIT006_14BIT002_14BIT012/abc.java
@@ -12,7 +12,8 @@ public class abc extends javax.swing.JApplet implements ActionListener {
 
     @Override
     public void init() {
-        /* Set the Nimbus look and feel */
+       // bhautik ni comment. lol lol llol
+    	/* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
